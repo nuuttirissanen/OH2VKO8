dae3c9b704e123d1d62e6bcf5292ef9ee0cfbf33
nuuttirissanen <nuuttiri@gmail.com>
2023-03-06 16:58:36 +0200

    2nd commit

--- src/main/java/com/github/nuuttirissanen/oh2vko8/tehtava5/Tehtava5.java
+++ src/main/java/com/github/nuuttirissanen/oh2vko8/tehtava5/Tehtava5.java
@@ -1,4 +1,19 @@
 package com.github.nuuttirissanen.oh2vko8.tehtava5;
 
 public class Tehtava5 {
+
+    public static void main(String[] args) {
+
+        Runnable tulostaA = new Tulosta('A');
+        Runnable tulostaB = new Tulosta('B');
+        Runnable tulostaC = new Tulosta('C');
+
+        Thread t1 = new Thread(tulostaA);
+        Thread t2 = new Thread(tulostaB);
+        Thread t3 = new Thread(tulostaC);
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
 }
--- src/main/java/com/github/nuuttirissanen/oh2vko8/tehtava5/Tulosta.java
+++ src/main/java/com/github/nuuttirissanen/oh2vko8/tehtava5/Tulosta.java
@@ -1,4 +1,17 @@
 package com.github.nuuttirissanen.oh2vko8.tehtava5;
 
-public class Tulosta {
+public class Tulosta implements Runnable{
+
+    private static final int kertaa = 101;
+    char c;
+
+    public Tulosta(char c) {
+        this.c = c;
+    }
+    @Override
+    public void run() {
+        for (int i = 1; i < kertaa; i++) {
+            System.out.println(this.c + "" + i);
+        }
+    }
 }
